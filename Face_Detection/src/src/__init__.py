20245c28bc5968b6b1a46272d79069fb5277a8d5
jmixon1100 <59591372+jmixon1100@users.noreply.github.com>
2020-04-13 15:33:59 -0400

    Another update
    got Gstreamer to connect to the program

--- Face_Detection/src/src/__init__.py
+++ Face_Detection/src/src/__init__.py
@@ -1,11 +1,28 @@
 import numpy as np
-import cv2
 
+import cv2
+from cv2 import cvtColor
+
+from PIL import ImageGrab
+import win32gui
+
+import time
 
 casc = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_default.xml')
 side_casc = cv2.CascadeClassifier('cascades/data/haarcascade_profileface.xml')
 
+templist = []
+otherlist = []
 
+def enum_win(hwnd, result):
+    #print(hwnd)
+    
+    winText = win32gui.GetWindowText(hwnd)
+    
+    #print(winText)
+    
+    otherlist.append((hwnd,winText))
+    
 def getDirections(xcords):
     if xcords > 380:
         return ("right")
@@ -13,78 +30,84 @@ def getDirections(xcords):
         return ("left")
     else:
         return ("center")
-webcam = int(input("Enter (0) if no local webcam, Enter (1) other wise): "))  
-cap = cv2.VideoCapture(webcam)
 
 dirfr = ""
 dirf = ""
 dirs = ""
-while(True):
-    #declare frame
-    ret, frame = cap.read()
+
+win32gui.EnumWindows(enum_win, templist)
+
+screen_hwnd = 0
+for (hwnd,winText) in otherlist:
+        if "GStreamer" in winText:
+            screen_hwnd = hwnd
+        
+start, end = 0,0    
+while(True):   
+    start = int(round(time.time() * 1000)) # <---------------------
+    
+    winPos = win32gui.GetWindowRect(screen_hwnd)
+    
+    cap = np.array(ImageGrab.grab(winPos))
+    
+    #declare frame    
+    frame = cvtColor(cap, cv2.COLOR_RGB2BGR)
     
     # inverted frame to capture side profile
     frame2 = cv2.flip(frame, 1)
-    
+     
     # set up gray scale variable 
     gs = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
+     
     # gray scale for inverted frame of side profile
     flipgs = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-    
+     
     #front facing cascade
     faces  = casc.detectMultiScale(gs, scaleFactor=1.5,minNeighbors=5)
-    
+     
     # inverted side cascade
     face_right = side_casc.detectMultiScale(flipgs, scaleFactor=1.5,minNeighbors=5)
-    
+     
     # side cascade 
     side = side_casc.detectMultiScale(gs, scaleFactor=1.5,minNeighbors=5)
     
     # loop for drawing box around faces detected on a side profile
     for (x,y,w,h) in face_right:
-        
+         
         # calculating x since the frame the faces being detected have been flipped
-        nx = 2 * h - x 
-        # formula for calculating x that has been flipped over a vertical line
-        
-        cv2.circle(frame,(nx + (w//2),y + (h//2)), 4,(0,0,255), -2)
-        #getting the center of each rectangle shown around a face
-        
+        nx = (2 * h) - x 
+        # formula for calculating x that has been flipped over a vertical line        
         cv2.rectangle(frame,(nx,y),(nx + w,y + h),(0,0,255),2)
-        
+         
         #block of code to not spam current location of face 
         dotPos = nx + (w//2)
         dir1 = getDirections(dotPos)
-        
+         
         if dir1 == dirfr:
             continue
         else:
             dirfr = dir1
             print (dirfr)
-            
+             
     for (x,y,w,h) in faces:
-        
-        cv2.circle(frame,(x + (w//2),y + (h//2)), 4,(0,0,255), -2)
         cv2.rectangle(frame,(x,y),(x + w,y + h),(255,0,0),2)
-        
+         
         dotPos = x + (w//2)
         dir1 = getDirections(dotPos)
-        
+         
         if dir1 == dirf:
             continue
         else:
             dirf = dir1
             print (dirf)
-        
-        
+         
+         
     for (x,y,w,h) in side:
-        cv2.circle(frame,(x + (w//2),y + (h//2)), 4,(0,0,255), -2)
         cv2.rectangle(frame,(x,y),(x + w,y + h),(0,255,0),2)
-        
+         
         dotPos = x + (w//2)
         dir1 = getDirections(dotPos)
-        
+         
         if dir1 == dirs:
             continue
         else:
@@ -97,5 +120,7 @@ while(True):
     #press q to close window
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-cap.release()
+    
+    end = int(round(time.time() * 1000)) # <--------------------
+    print(end - start)
 cv2.destroyAllWindows()
