37a8a86de0c4dc9439c91ab78f509a438f3bf7ef
jmixon1100 <59591372+jmixon1100@users.noreply.github.com>
2020-04-14 17:47:08 -0400

    added servo support 4/14/20

--- controller.ino
+++ controller.ino
@@ -1,9 +1,12 @@
+#include <Servo.h>
+Servo piv;
 int newData;
 void setup() {
   Serial.begin(9600);
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
+  piv.attach (9);
   Serial.setTimeout(10);
 }
 void loop() {
@@ -12,21 +15,24 @@ void loop() {
 void serialEvent(){
   newData = Serial.read();
     if(newData == '1'){
+      piv.write(180);
       digitalWrite(2,HIGH);
       digitalWrite(4,LOW);
       digitalWrite(3,LOW);
     }else if(newData ==  '2'){
+      piv.write(0);
       digitalWrite(4,HIGH);
       digitalWrite(2,LOW);
       digitalWrite(3,LOW);
     }else if(newData == '3'){
+      piv.write(90);
       digitalWrite(3,HIGH);
       digitalWrite(4,LOW);
       digitalWrite(2,LOW);
     }else{
+      piv.write(90);
       digitalWrite(3,LOW);
       digitalWrite(4,LOW);
       digitalWrite(2,LOW);
     }
 }
-
